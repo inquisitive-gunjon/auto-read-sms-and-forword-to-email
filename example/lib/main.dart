03415240c681b77f4a6899479f2d81a707cea59e
excelitai-gunjon <gunjon@excelitai.com>
2022-08-28 18:02:18 +0600

    done

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server.dart';
 import 'dart:async';
 import 'package:telephony/telephony.dart';
+import 'package:telephony_example/service.dart';
 
 onBackgroundMessage(SmsMessage message) {
   debugPrint("onBackgroundMessage called");
@@ -17,6 +20,19 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String _message = "";
+  int? id;
+  String? address;
+  String? body;
+  int? date;
+  int? dateSent;
+  bool? read;
+  bool? seen;
+  String? subject;
+  int? subscriptionId;
+  int? threadId;
+  SmsType? type;
+  SmsStatus? status;
+  String? serviceCenterAddress;
   final telephony = Telephony.instance;
 
   @override
@@ -28,7 +44,20 @@ class _MyAppState extends State<MyApp> {
   onMessage(SmsMessage message) async {
     setState(() {
       _message = message.body ?? "Error reading message body.";
+      id = message.id;
+      address = message.address;
+      date = message.date;
+      dateSent = message.dateSent;
+      read = message.read;
+      seen = message.seen;
+      subject = message.subject;
+      subscriptionId = message.subscriptionId;
+      threadId = message.threadId;
+      type = message.type;
+      status = message.status;
+      serviceCenterAddress = message.serviceCenterAddress;
     });
+    await senEmail(_message);
   }
 
   onSendStatus(SendStatus status) {
@@ -53,6 +82,29 @@ class _MyAppState extends State<MyApp> {
 
     if (!mounted) return;
   }
+  Future senEmail(String sms)async{
+    final user= await GoogleAuthApi.signIn();
+    if(user==null)return;
+
+    //final email = 'gunjon.cse@gmail.com';
+    final email = user.email;
+    final auth = await user.authentication;
+    final accessToken=auth.accessToken!;
+    final smtpServer = gmailSaslXoauth2(email, accessToken);
+
+    final message = Message()
+    ..from=Address(email,'Gunjon')
+    ..recipients=['gunjon.cse@gmail.com']
+    ..subject='from sms'
+    ..text=sms;
+
+    try{
+      await send(message, smtpServer);
+      print("success");
+    }on MailerException catch(e){
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -64,7 +116,31 @@ class _MyAppState extends State<MyApp> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Center(child: Text("Latest received SMS: $_message")),
+      // String _message = "";
+      // int? id;
+      // String? address;
+      // String? body;
+      // int? date;
+      // int? dateSent;
+      // bool? read;
+      // bool? seen;
+      // String? subject;
+      // int? subscriptionId;
+      // int? threadId;
+      // SmsType? type;
+      // SmsStatus? status;
+      // String? serviceCenterAddress;
+          Center(child: Text("Latest received SMS id: $id")),
+          Center(child: Text("Latest received SMS address: $address")),
+          Center(child: Text("Latest received SMS message: $_message")),
+          Center(child: Text("Latest received SMS date: ${DateTime.now()}")),
+          Center(child: Text("Latest received SMS dateSent: $dateSent")),
+          Center(child: Text("Latest received SMS read: $read")),
+          Center(child: Text("Latest received SMS subject: $subject")),
+          Center(child: Text("Latest received SMS subscriptionId: $subscriptionId")),
+          Center(child: Text("Latest received SMS type: $type")),
+          Center(child: Text("Latest received SMS status: $status")),
+          Center(child: Text("Latest received SMS serviceCenterAddress: $serviceCenterAddress")),
           TextButton(
               onPressed: () async {
                 await telephony.openDialer("123413453");
---
+++ example/lib/service.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:google_sign_in/google_sign_in.dart';
+
+class GoogleAuthApi {
+  static final _googleSignIn = GoogleSignIn();
+
+  static Future<GoogleSignInAccount?> signIn()async{
+    if(await _googleSignIn.isSignedIn()){
+      return _googleSignIn.currentUser;
+    }else{
+      return await _googleSignIn.signIn();
+    }
+  }
+}
